d691c5a3ba5dbd37ae8a3f03209670958edc5fef
imtiyaz786 <iali75637@gmail.com>
2021-08-09 10:18:27 +0530

    week2 work

--- src/mywork/Validation.java
+++ src/mywork/Validation.java
@@ -3,7 +3,7 @@ package mywork;
 public class Validation {
 	
 	public boolean validateEmail(String email) throws InvalidEmailException{
-		if(email .matches("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b"))
+		if(email != null && email .matches("[a-z]{6}[A-Z]{3}[0-9]{5}[*]{1}"))
 			return true;
 		else
 			throw new InvalidEmailException("Entered email : " + email + " is invalid");
@@ -12,7 +12,7 @@ public class Validation {
 	}
 	
 	public boolean validateAadhar(String aadhar) throws InvalidAadharException {
-		if(aadhar.matches("[2-9]{1}[0-9]{3}\\s[0-9]{4}\\s[0-9]{4}$"))
+		if(aadhar!=null && aadhar.matches("[A-Z]{3}[a-z]{3}[0-9]{8}"))
 			return true;
 		else
 			throw new InvalidAadharException("Entered aadhar : " + aadhar + " is invalid");		
